66278e6a902498219adbe474bc2e8174ac268774
changlin158023 <296576059@qq.com>
2015-06-05 14:36:44 +0800

    B1

--- agree/src/com/biju/BaseActivity.java
+++ agree/src/com/biju/BaseActivity.java
@@ -13,7 +13,7 @@ public abstract class BaseActivity extends Activity {
 	}
 
 	
-//	.........................
+//	.........................1
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
