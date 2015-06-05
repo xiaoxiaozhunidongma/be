34edb08ef71ceccbd357749f0e6175b6b6f261c1
xiaoxiaozhunidongma <460587126@qq.com>
2015-06-05 14:24:37 +0800

    test

--- agree/src/com/biju/Interface.java
+++ agree/src/com/biju/Interface.java
@@ -111,7 +111,7 @@ public class Interface {
 	
 	public static void requestError(VolleyError error) {
 	
-		//1
+		//1............
 	}
 	
 	public Map<String, String> packParams(Object classObject , String interfaceType) {
