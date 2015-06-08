9cee0bd9accca063f0f75cdeb42310cd5591a563
xiaoxiaozhunidongma <460587126@qq.com>
2015-06-08 16:41:41 +0800

    我帮你修理了

--- agree/src/com/biju/login/LoginActivity.java
+++ agree/src/com/biju/login/LoginActivity.java
@@ -65,7 +65,9 @@ public class LoginActivity extends Activity implements OnClickListener {
 
 					@Override
 					public void run() {
-						drawable.stop();
+						if(drawable!=null){
+							drawable.stop();
+						}
 						Intent intent = new Intent(LoginActivity.this,
 								MainActivity.class);
 						startActivity(intent);
