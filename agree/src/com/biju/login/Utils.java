f43fa559e46e713ac36631d0f2ab331d8c56fba6
xiaoxiaozhunidongma <460587126@qq.com>
2015-06-09 15:21:08 +0800

    15

---
+++ agree/src/com/biju/login/Utils.java
@@ -0,0 +1,31 @@
+package com.biju.login;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Utils
+{
+    
+    public static Bitmap decodeSampledBitmap(String path, int sample) 
+    {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+
+        // Calculate inSampleSize
+        options.inSampleSize = sample;
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(path, options);
+    }
+    
+    public static String secondToDate(long second)
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        java.util.Date dt = new Date(second * 1000);
+        return sdf.format(dt);
+    }
+}
